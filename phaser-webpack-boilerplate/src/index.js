1436f489333a274fedb934c1f566b9009f93ed45
LadySunbreaker21 <elmartinez@cocoschool.mx>
2023-05-08 17:53:41 -0600

    Cambio de movimiento y arreglo de fisicas.

--- phaser-webpack-boilerplate/src/index.js
+++ phaser-webpack-boilerplate/src/index.js
@@ -7,6 +7,8 @@ class MainScene extends Phaser.Scene {
     super({ key: "MainScene" });
     this.snake = null;
     this.fruit = null;
+    this.score = 0;
+    this.scoreText = null;
   }
 
   preload() {
@@ -22,44 +24,76 @@ class MainScene extends Phaser.Scene {
     this.snake.setScale(0.5);
     this.physics.add.existing(this.snake);
 
-    
-    const keyboard = this.input.keyboard;
-    keyboard.on("keydown-W", () => this.moveUp());
-    keyboard.on("keydown-A", () => this.moveLeft());
-    keyboard.on("keydown-S", () => this.moveDown());
-    keyboard.on("keydown-D", () => this.moveRight());
+    // Eventos de Movimiento
+    this.input.keyboard.on("keydown-D", this.moveRight, this);
 
-   //Poner fruta de forma aleatoria.
+    // Evento 02
+    this.input.keyboard.on("keydown-A", this.moveLeft, this);
+
+    // Evento 03
+    this.input.keyboard.on("keydown-W", this.moveUp, this);
+
+     // Evento 03
+     this.input.keyboard.on("keydown-S", this.moveDown, this);
+    
+
+
+    // Poner fruta de forma aleatoria
     this.fruit = this.physics.add.sprite(Phaser.Math.Between(0, this.game.config.width), Phaser.Math.Between(0, this.game.config.height), "fruit");
+
+    // Colision entre la serpiente y la fruta
+    this.physics.add.collider(this.snake, this.fruit, this.collectFruit, null, this);
+
+    // Puntaje
+    this.scoreText = this.add.text(10, 10, `Score: ${this.score}`, { fontSize: "32px", fill: "#000" });
   }
 
   update() {
-   
+
+     // Verificar si la serpiente se sale de la pantalla
+  if (this.snake.x < 0 || this.snake.x > this.game.config.width || this.snake.y < 0 || this.snake.y > this.game.config.height) {
+     this.score = 0;
+     this.scene.restart();
+    
+  
   }
+}
 
-
-  //Movimiento
+  // Movimiento general 
   moveRight() {
-    this.snake.setVelocityX(SNAKE_VELOCITY);
-    this.snake.setVelocityY(0);
+    this.snake.body.velocity.x = SNAKE_VELOCITY;
+    this.snake.body.velocity.y = 0;
   }
 
   moveLeft() {
-    this.snake.setVelocityX(-SNAKE_VELOCITY);
-    this.snake.setVelocityY(0);
+    this.snake.body.velocity.x = - SNAKE_VELOCITY;
+    this.snake.body.velocity.y = 0;
   }
 
   moveUp() {
-    this.snake.setVelocityX(0);
-    this.snake.setVelocityY(-SNAKE_VELOCITY);
+    this.snake.body.velocity.y =  - SNAKE_VELOCITY;
+    this.snake.body.velocity.x = 0;
   }
 
   moveDown() {
-    this.snake.setVelocityX(0);
-    this.snake.setVelocityY(SNAKE_VELOCITY);
+    this.snake.body.velocity.y =  SNAKE_VELOCITY;
+    this.snake.body.velocity.x = 0;
   }
 
+   // Función para recoger la fruta
+   collectFruit() {
+   
+    this.score += 10;
+    this.scoreText.setText(`Score: ${this.score}`);
+
+    // Mover la fruta a una nueva posición aleatoria y detener movimiento por sus fisicas
+    this.fruit.setPosition(Phaser.Math.Between(0, this.game.config.width), Phaser.Math.Between(0, this.game.config.height));
+    this.fruit.body.velocity.x= 0;
+    this.fruit.body.velocity.y= 0;
+    
 }
+}
+
 
 const config = {
   type: Phaser.AUTO,
